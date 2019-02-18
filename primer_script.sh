ce010f3c599052f4fbe1d67d08ff02de0240e870
saralpla <saralpla@gmail.com>
2019-02-18 16:52:49 -0600

    Agregar primer script

---
+++ primer_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hola,estudiante del taller de GNU/Linux"
+
+#Lectura interactiva del nombre
+echo "Ingrese su nombre:"
+read name
+#Mostrar la variable name
+echo Su nombre es: $name
+
+
+
